3dffec0a2704cad8fab2009c7fa1a852410c5007
DDX <ddxman@mail.ru>
2023-09-24 11:50:52 +0300

    Тест Пхп Шторм с гитом

---
+++ www/Test/margin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<style>
+    .container{
+        background-color: violet;
+    }
+</style>
+<body>
+    <div class="container w-100 mx-auto ">
+<p class="w-25 mx-auto mx-0 my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda nostrum obcaecati, vero consectetur dignissimos doloribus, odit odio rerum, dicta at sint? Sequi tempore expedita iste odit, dolor officia perferendis, deserunt ratione illo atque vel omnis? Fugit sed doloremque officiis libero praesentium quaerat. At inventore qui dolore, minima quis eaque maiores placeat similique deserunt nostrum eos veritatis eveniet asperiores officiis iusto quos? Fuga nesciunt culpa provident quam quas optio iste voluptates quibusdam perspiciatis. Iusto temporibus sapiente officia quo, error accusamus blanditiis distinctio ratione magnam fuga deleniti repudiandae facilis fugit aliquam inventore alias suscipit in similique culpa cumque esse. Facere, minus dolor.</p>
+
+</div>
+</body>
+</html>
